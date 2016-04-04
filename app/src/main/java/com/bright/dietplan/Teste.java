690a426cb463555d0084389a844886e51dd8660b
David Julião <djuliao08@gmail.com>
2016-04-04 13:08:52 +0100

    testestestestestes

---
+++ app/src/main/java/com/bright/dietplan/Teste.java
@@ -0,0 +1,13 @@
+package com.bright.dietplan;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Teste extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teste2);
+    }
+}
