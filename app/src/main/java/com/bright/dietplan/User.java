b0375eac4ed81e376d1dbcf1c64a56fbbf6564bd
David Julião <djuliao08@gmail.com>
2016-04-04 18:56:06 +0100

    #1

--- app/src/main/java/com/bright/dietplan/User.java
+++ app/src/main/java/com/bright/dietplan/User.java
@@ -3,11 +3,11 @@ package com.bright.dietplan;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Teste extends AppCompatActivity {
+public class User extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_teste2);
+        setContentView(R.layout.activity_user);
     }
 }
